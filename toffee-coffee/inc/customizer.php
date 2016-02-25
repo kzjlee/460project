a3a804eba5170cd4205369b8c4ffc008bfc0f348
kzjlee <kzjlee@gmail.com>
2016-02-25 18:00:36 -0500

    Added files via upload

---
+++ toffee-coffee/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * toffee-coffee Theme Customizer.
+ *
+ * @package toffee-coffee
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function toffee_coffee_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'toffee_coffee_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function toffee_coffee_customize_preview_js() {
+	wp_enqueue_script( 'toffee_coffee_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'toffee_coffee_customize_preview_js' );
