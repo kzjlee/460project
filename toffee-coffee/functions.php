7490f6140920eb256d7581b5894ab7e50e54818a
kzjlee <kzjlee@gmail.com>
2016-03-11 00:47:31 -0500

    Menu in progress

--- toffee-coffee/functions.php
+++ toffee-coffee/functions.php
@@ -45,6 +45,7 @@ function toffee_coffee_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Primary', 'toffee-coffee' ),
+		'secondary' => esc_html__( 'secondary', 'toffee-coffee' ),
 	) );
 
 	/*
--- toffee-coffee/header.php
+++ toffee-coffee/header.php
@@ -74,7 +74,9 @@
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'toffee-coffee' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+
+
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'menu_class'     => 'nav-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
