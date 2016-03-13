5deb37d93012e62f50c629b6c87288dae43dca1c
kzjlee <kzjlee@gmail.com>
2016-03-13 15:48:50 -0400

    Comment template and styling comments

--- toffee-coffee/comments.php
+++ toffee-coffee/comments.php
@@ -27,10 +27,9 @@ if ( post_password_required() ) {
 	if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'toffee-coffee' ) ),
-					number_format_i18n( get_comments_number() ),
-					'<span>' . get_the_title() . '</span>'
+				printf( // WPCS: XSS OK. -- changing what it prints on the comments
+					esc_html( _nx( 'One Comment', '%1$s Comments', get_comments_number(), 'comments title', 'toffee-coffee' ) ),
+					number_format_i18n( get_comments_number() )
 				);
 			?>
 		</h2>
