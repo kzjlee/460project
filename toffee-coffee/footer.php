9a29042b57d001052af446148b08c2564167ef2b
kzjlee <kzjlee@gmail.com>
2016-03-14 18:59:46 -0400

    Footer Widget

--- toffee-coffee/footer.php
+++ toffee-coffee/footer.php
@@ -13,7 +13,34 @@
 
 	</div><!-- #content -->
 
+
+
 	<footer id="colophon" class="site-footer" role="contentinfo">
+
+				<div id="footer-sidebar" class="secondary">
+				<div id="footer-sidebar1">
+					<?php
+						if(is_active_sidebar('footer-sidebar-1')){
+						dynamic_sidebar('footer-sidebar-1');
+						}
+					?>
+				</div>
+				<div id="footer-sidebar2">
+					<?php
+						if(is_active_sidebar('footer-sidebar-2')){
+						dynamic_sidebar('footer-sidebar-2');
+						}
+					?>
+				</div>
+				<div id="footer-sidebar3">
+					<?php
+						if(is_active_sidebar('footer-sidebar-3')){
+						dynamic_sidebar('footer-sidebar-3');
+						}
+					?>
+				</div>
+				</div>
+
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'toffee-coffee' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'toffee-coffee' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
