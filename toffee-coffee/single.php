73f2d4d4b0575c3498c5725cd14ad936f43d452b
AlexTsai <Alex-Tsai@Tsungs-MacBook-Pro.local>
2016-03-14 01:11:22 -0400

    Move navigation position
    Move the post navigation below the comment area

--- toffee-coffee/single.php
+++ toffee-coffee/single.php
@@ -17,12 +17,15 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content', 'single' );
 
+
+
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif;
 
 			the_post_navigation();
+			
 
 
 		endwhile; // End of the loop.
