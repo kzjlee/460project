0b15fabca3c3b09d65fc6a3465ea96b9f3899851
kzjlee <kzjlee@gmail.com>
2016-03-14 22:40:09 -0400

    nav buttons styling and options page

--- toffee-coffee/inc/options.php
+++ toffee-coffee/inc/options.php
@@ -1,119 +1,100 @@
 <?php
-	
-function cd_add_submenu() {
-		add_submenu_page( 'themes.php', 'Coffee Toffee Options Page', 'Theme Options', 'manage_options', 'my-sub-menu', 'my_theme_options_page');
-	}
-add_action( 'admin_menu', 'cd_add_submenu' );
+// Followed options typography tutorial on http://theme.fm/2011/08/providing-typography-options-in-your-wordpress-themes-1576/
 
+add_action( 'admin_menu', 'my_admin_menu' );
+function my_admin_menu() {
+    add_theme_page( 'My Theme Options', 'My Theme Options', 'edit_theme_options', 'my-theme-options', 'my_theme_options' );
+}
+function my_theme_options() {
+?>
+    <div class="wrap">
+        <div><br></div>
+        <h2>Coffee Toffee Theme Options</h2>
 
-function cd_settings_init() { 
-	register_setting( 'theme_options', 'cd_options_settings' );
-	
-	add_settings_section(
-		'cd_options_page_section', 
-		'Your section description', 
-		'cd_options_page_section_callback', 
-		'theme_options'
-	);
-	
-	function cd_options_page_section_callback() { 
-		echo 'A description and detail about the section.';
-	}
-
-	add_settings_field( 
-		'cd_text_field', 
-		'Enter your text', 
-		'cd_text_field_render', 
-		'theme_options', 
-		'cd_options_page_section' 
-	);
-
-	add_settings_field( 
-		'cd_checkbox_field', 
-		'Check your preference', 
-		'cd_checkbox_field_render', 
-		'theme_options', 
-		'cd_options_page_section'  
-	);
-
-	add_settings_field( 
-		'cd_radio_field', 
-		'Choose an option', 
-		'cd_radio_field_render', 
-		'theme_options', 
-		'cd_options_page_section'  
-	);
-	
-	add_settings_field( 
-		'cd_textarea_field', 
-		'Enter content in the textarea', 
-		'cd_textarea_field_render', 
-		'theme_options', 
-		'cd_options_page_section'  
-	);
-	
-	add_settings_field( 
-		'cd_select_field', 
-		'Choose from the dropdown', 
-		'cd_select_field_render', 
-		'theme_options', 
-		'cd_options_page_section'  
-	);
-
-	function cd_text_field_render() { 
-		$options = get_option( 'cd_options_settings' );
-		?>
-		<input type="text" name="cd_options_settings[cd_text_field]" value="<?php if (isset($options['cd_text_field'])) echo $options['cd_text_field']; ?>" />
-		<?php
-	}
-	
-	function cd_checkbox_field_render() { 
-		$options = get_option( 'cd_options_settings' );
-	?>
-		<input type="checkbox" name="cd_options_settings[cd_checkbox_field]" <?php if (isset($options['cd_checkbox_field'])) checked( 'on', ($options['cd_checkbox_field']) ) ; ?> value="on" />
-		<label>Turn it On</label> 
-		<?php	
-	}
-	
-	function cd_radio_field_render() { 
-		$options = get_option( 'cd_options_settings' );
-		?>
-		<input type="radio" name="cd_options_settings[cd_radio_field]" <?php if (isset($options['cd_radio_field'])) checked( $options['cd_radio_field'], 1 ); ?> value="1" /> <label>Option One</label><br />
-		<input type="radio" name="cd_options_settings[cd_radio_field]" <?php if (isset($options['cd_radio_field'])) checked( $options['cd_radio_field'], 2 ); ?> value="2" /> <label>Option Two</label><br />
-		<input type="radio" name="cd_options_settings[cd_radio_field]" <?php if (isset($options['cd_radio_field'])) checked( $options['cd_radio_field'], 3 ); ?> value="3" /> <label>Option Three</label>
-		<?php
-	}
-	
-	function cd_textarea_field_render() { 
-		$options = get_option( 'cd_options_settings' );
-		?>
-		<textarea cols="40" rows="5" name="cd_options_settings[cd_textarea_field]"><?php if (isset($options['cd_textarea_field'])) echo $options['cd_textarea_field']; ?></textarea>
-		<?php
-	}
-
-	function cd_select_field_render() { 
-		$options = get_option( 'cd_options_settings' );
-		?>
-		<select name="cd_options_settings[cd_select_field]">
-			<option value="1" <?php if (isset($options['cd_select_field'])) selected( $options['cd_select_field'], 1 ); ?>>Option 1</option>
-			<option value="2" <?php if (isset($options['cd_select_field'])) selected( $options['cd_select_field'], 2 ); ?>>Option 2</option>
-		</select>
-	<?php
-	}
-	
-	function my_theme_options_page(){ 
-		?>
-		<form action="options.php" method="post">
-			<h2>My Awesome Options Page</h2>
-			<?php
-			settings_fields( 'theme_options' );
-			do_settings_sections( 'theme_options' );
-			submit_button();
-			?>
-		</form>
-		<?php
-	}
-
+        <form method="post" action="options.php">
+            <?php wp_nonce_field( 'update-options' ); ?>
+            <?php settings_fields( 'my-options' ); ?>
+            <?php do_settings_sections( 'my-options' ); ?>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+<?php
 }
 
-add_action( 'admin_init', 'cd_settings_init' );
+add_action( 'admin_init', 'my_register_admin_settings' );
+function my_register_admin_settings() {
+    register_setting( 'my-options', 'my-options' );
+
+    // Settings fields and sections
+    add_settings_section( 
+        'section_typography', //the id
+        'Typography Options', // section title
+        'my_section_typography', // $callback function 
+        'my-options' ); //page (matches menu_slug set in add_submenu_page)
+
+     //the function created in the add_settings_section
+    add_settings_field( 'primary-font', 'Primary Font', 'my_field_primary_font', 'my-options', 'section_typography' );
+}
+
+function my_section_typography() {
+    echo 'Choose your font here.';
+}
+
+function my_field_primary_font() {
+    $options = (array) get_option( 'my-options' );
+    $fonts = get_my_available_fonts();
+    $current_font = 'Roboto';
+
+    if ( isset( $options['primary-font'] ) )
+        $current_font = $options['primary-font'];
+
+    ?>
+        <select name="my-options[primary-font]">
+        <?php foreach( $fonts as $font_key => $font ): ?>
+            <option <?php selected( $font_key == $current_font ); ?> value="<?php echo $font_key; ?>"><?php echo $font['name']; ?></option>
+        <?php endforeach; ?>
+        </select>
+    <?php
+}
+function get_my_available_fonts() {
+    $fonts = array(
+        'open-sans' => array(
+            'name' => 'Cabin Sketch',
+            'import' => '',
+            'css' => "font-family: 'Cabin Sketch', cursive;"
+        ),
+        'lato' => array(
+            'name' => 'Roboto',
+            'import' => '',
+            'css' => "font-family: 'Roboto', sans-serif;"
+        ),
+        'arial' => array(
+            'name' => 'Arial',
+            'import' => '',
+            'css' => "font-family: Arial, sans-serif;"
+        )
+    );
+
+    return apply_filters( 'my_available_fonts', $fonts );
+}
+
+
+
+add_action( 'wp_head', 'my_wp_head' );
+function my_wp_head() {
+    $options = (array) get_option( 'my-options' );
+    $fonts = get_my_available_fonts();
+    $current_font_key = 'arial';
+
+    if ( isset( $options['primary-font'] ) )
+        $current_font_key = $options['primary-font'];
+
+    if ( isset( $fonts[ $current_font_key ] ) ) {
+        $current_font = $fonts[ $current_font_key ];
+
+        echo '<style>';
+        echo $current_font['import'];
+        echo 'body * { ' . $current_font['css'] . ' } ';
+        echo '</style>';
+    }
+}
